79bf25e792795788ea2e60a5c038a4eef14e231b
Nakzz <naqibprio10@gmail.com>
2018-12-08 02:22:44 +0000

    Testing node

--- server2.js
+++ server2.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'pug');
-
 app.get('/test', (req,res)=> {
 res.send('Hello World!');
 });
 
 app.get('/', (req,res)=> {
-res.send('Req: update'+ res);
+res.send('Req: '+ res);
 //console.dir(res);
 });
 
-const server = app.listen(7002, ()=> {
+const server = app.listen(7001, ()=> {
 console.log(`Express running at port ${server.address().port}`);
 });
